ef6357ca9d73a6d0484e3de8fccfa5386140200d
Silencefbb0108 <37800285+Silencefbb0108@users.noreply.github.com>
2019-06-20 13:57:33 +0800

    Update graph_optimize_map.cpp

--- graph_optimize_map.cpp
+++ graph_optimize_map.cpp
@@ -12,6 +12,116 @@ using namespace cv;
 #define MAXN 90000
 
 #define PI 3.1415926535897932384626
+#define mul 4
+
+//去除二值图像边缘的突出部
+//uthreshold、vthreshold分别表示突出部的宽度阈值和高度阈值
+//type代表突出部的颜色，0表示黑色，1代表白色 
+void delete_jut(cv::Mat& src, cv::Mat& dst, int uthreshold, int vthreshold, int type) {
+
+    src.copyTo(dst);
+    int height = dst.rows;
+    int width = dst.cols;
+    int k;  //用于循环计数传递到外部
+    for (int i = 0; i < height - 1; i++)
+    {
+	uchar* p = dst.ptr<uchar>(i);
+	for (int j = 0; j < width - 1; j++)
+	{
+	    if (type == 0)
+	    {
+		//行消除
+		if (p[j] == 255 && p[j + 1] == 0)
+		{
+		    if (j + uthreshold >= width)
+		    {
+			for (k = j + 1; k < width; k++)
+			    p[k] = 255;
+		    }
+		    else
+		    {
+			for (k = j + 2; k <= j + uthreshold; k++)
+			{
+			    if (p[k] == 255) break;
+			}
+			if (p[k] == 255)
+			{
+			    for (int h = j + 1; h < k; h++)
+				p[h] = 255;
+			}
+		    }
+		}
+		//列消除
+		if (p[j] == 255 && p[j + width] == 0)
+		{
+		    if (i + vthreshold >= height)
+		    {
+			for (k = j + width; k < j + (height - i)*width; k += width)
+			    p[k] = 255;
+		    }
+		    else
+		    {
+			for (k = j + 2 * width; k <= j + vthreshold*width; k += width)
+			{
+			    if (p[k] == 255) break;
+			}
+			if (p[k] == 255)
+			{
+			    for (int h = j + width; h < k; h += width)
+				p[h] = 255;
+			}
+		    }
+		}
+	    }
+	    else  //type = 1
+	    {
+		//行消除
+		if (p[j] == 0 && p[j + 1] == 255)
+		{
+		    if (j + uthreshold >= width)
+		    {
+			for (k = j + 1; k < width; k++)
+			    p[k] = 0;
+		    }
+		    else
+		    {
+			for (k = j + 2; k <= j + uthreshold; k++)
+			{
+			    if (p[k] == 0) break;
+			}
+			if (p[k] == 0)
+			{
+			    for (int h = j + 1; h < k; h++)
+				p[h] = 0;
+			}
+		    }
+		}
+		//列消除
+		if (p[j] == 0 && p[j + width] == 255)
+		{
+		    if (i + vthreshold >= height)
+		    {
+			for (k = j + width; k < j + (height - i)*width; k += width)
+			    p[k] = 0;
+		    }
+		    else
+		    {
+			for (k = j + 2 * width; k <= j + vthreshold*width; k += width)
+			{
+			    if (p[k] == 0) break;
+			}
+		        if (p[k] == 0)
+			{
+			    for (int h = j + width; h < k; h += width)
+				p[h] = 0;
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+}
 
 //CheckMode: 0代表去除黑区域，1代表去除白区域; NeihborMode：0代表4邻域，1代表8邻域;  
 void RemoveSmallRegion(cv::Mat& Src, cv::Mat& Dst, int AreaLimit, int CheckMode, int NeihborMode)  
@@ -159,6 +269,23 @@ void imfillholes(cv::Mat &src)
     hierarchy.clear();
 }
 
+vector<cv::Point2i> getRotatePoint(cv::Mat srcImage, vector<cv::Point2i> Points, const cv::Point rotate_center, const double angle) {
+	vector<cv::Point2i> dstPoints;
+	int x1 = 0, y1 = 0;
+	int row = srcImage.rows;
+	for (size_t i = 0; i < Points.size(); i++)
+	{
+		x1 = Points.at(i).x;
+		y1 = row - Points.at(i).y;
+		int x2 = rotate_center.x;
+		int y2 = row - rotate_center.y;
+		int x = cvRound((x1 - x2)*cos(PI / 180.0 * angle) - (y1 - y2)*sin(PI / 180.0 * angle) + x2);
+		int y = cvRound((x1 - x2)*sin(PI / 180.0 * angle) + (y1 - y2)*cos(PI / 180.0 * angle) + y2);
+		y = row - y;
+		dstPoints.push_back(Point2i(x, y));
+	}
+	return dstPoints;
+}
 
 cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int len, float x0, float y0, float f) {
 
@@ -171,12 +298,15 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
     
     cv::Mat src2 = cv::Mat::zeros(src.size(), CV_8UC1);
     src2 = (80 <= src & src <= 100);
-    imshow("src2", src2);
+    //imshow("src2", src2);
 
     if(countNonZero(src2) == 0) {
 
        cv::Mat zoom;
-       cv::resize(src, zoom, cv::Size(), 4*f, 4*f, cv::INTER_NEAREST);
+       cv::resize(src, zoom, cv::Size(), mul*f, mul*f, cv::INTER_NEAREST);
+       cv::medianBlur(zoom, zoom, 9);
+       cv::medianBlur(zoom, zoom, 5);
+       cv::medianBlur(zoom, zoom, 3);
 
        cv::Mat color_map = cv::Mat::zeros(zoom.size(), CV_8UC3);
 
@@ -199,6 +329,7 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
                }
            }
        }
+       cv::blur(color_map, color_map, cv::Size(3,3));
 
        return color_map;
     } 
@@ -246,24 +377,24 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
                 }
             }
         }
-        imshow("blank_area", blank_area);
+        //imshow("blank_area", blank_area);
 
 	cv::Mat known_area2;
 	bitwise_or(blank_area, src2, known_area2);
-        imshow("known_area2", known_area2);
+        //imshow("known_area2", known_area2);
         cv::Mat element = getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
         erode(known_area2, known_area2, element);
 	dilate(known_area2, known_area2, element);
-        imshow("pre-fill", known_area2);
+        //imshow("pre-fill", known_area2);
 
 	imfillholes(known_area2);
-        imshow("post-fill", known_area2);
+        //imshow("post-fill", known_area2);
 	src = ~src;
-        imshow("anti-src", src);
+        //imshow("anti-src", src);
 	bitwise_and(known_area2, src, known_area2);
-        imshow("post-and", known_area2);
+        //imshow("post-and", known_area2);
 	known_area2 = ~known_area2;
-        imshow("anti-known_area2", known_area2);
+        //imshow("anti-known_area2", known_area2);
 
         cv::Mat B_known_area2 = cv::Mat::zeros(src.size(), CV_8UC1);
         B_known_area2 = (known_area2 < 80);
@@ -287,9 +418,9 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
         BL_known_area2 = (80 <= known_area2 & known_area2 <= 100);
         cv::Mat BLA_known_area2 = cv::Mat::zeros(src.size(), CV_8UC1);
         bitwise_or(blank_area2, BL_known_area2, BLA_known_area2);
-	cv::Mat element2 = getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
-        erode(BLA_known_area2, BLA_known_area2, element2);
-	dilate(BLA_known_area2, BLA_known_area2, element2);
+	//cv::Mat element2 = getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
+        //erode(BLA_known_area2, BLA_known_area2, element2);
+	//dilate(BLA_known_area2, BLA_known_area2, element2);
         cv::Mat BLAN_known_area2 = cv::Mat::zeros(src.size(), CV_8UC1);
         bitwise_and(BLA_known_area2, src, BLAN_known_area2);
 	BLAN_known_area2 = ~BLAN_known_area2;
@@ -397,12 +528,13 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
 	       ms4.clear();
             }
         }
-        imshow("BLAN_known_area2-", BLAN_known_area2);
+        //imshow("BLAN_known_area2", BLAN_known_area2);
 
         cv::Mat BLANK2 = cv::Mat::zeros(src.size(), CV_8UC1);
         BLANK2 = (BLAN_known_area2 < 80);
         cv::Mat BLANK3 = cv::Mat::zeros(src.size(), CV_8UC1);
         BLANK2.copyTo(BLANK3);
+        //imshow("BLANK3", BLANK3);
 
         for(int i = 1; i < src.rows-1; ++i) {
             for(int j = 1; j < src.cols-1; ++j) {
@@ -432,24 +564,24 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
                 }
             }
         }
-        imshow("BLANK3", BLANK3);
-        imshow("BLANK2", BLANK2);
+        //imshow("BLANK2", BLANK2);
 
         cv::Mat BARRIER2 = cv::Mat::zeros(src.size(), CV_8UC1);
         BARRIER2 = (80 <= BLAN_known_area2 & BLAN_known_area2 <= 100);
         cv::Mat BARRIER3 = cv::Mat::zeros(src.size(), CV_8UC1);
         bitwise_and(BLANK2, BARRIER2, BARRIER3);
-        imshow("BARRIER3", BARRIER3);
-        cv::Mat NOT_KNOWN = cv::Mat::zeros(src.size(), CV_8UC1);
-        bitwise_or(BLANK3, BARRIER3, NOT_KNOWN);
-        imshow("NOT_KNOWN", NOT_KNOWN);
+        //imshow("BARRIER3", BARRIER3);
+        cv::Mat NOT_KNOWN0 = cv::Mat::zeros(src.size(), CV_8UC1);
+        bitwise_or(BLANK3, BARRIER3, NOT_KNOWN0);
+        //imshow("NOT_KNOWN0", NOT_KNOWN0);
+	cv::Mat NOT_KNOWN = cv::Mat::zeros(src.size(), CV_8UC1);
+	delete_jut(NOT_KNOWN0, NOT_KNOWN, 3, 3, 1);
         bitwise_and(NOT_KNOWN, ~BLAN_known_area2, NOT_KNOWN);
-        imshow("NOT_KNOWN2", NOT_KNOWN);
+        //imshow("NOT_KNOWN2", NOT_KNOWN);
         //RemoveSmallRegion(NOT_KNOWN, NOT_KNOWN, 100, 0, 1);
         //imshow("NOT_KNOWN3", NOT_KNOWN);
 	NOT_KNOWN = ~NOT_KNOWN;
-        imshow("NOT_KNOWN4", NOT_KNOWN);
-	cout << NOT_KNOWN << endl;
+        //imshow("NOT_KNOWN4", NOT_KNOWN);
 
         int flag_up = 0;
         for(int i = 0; i < NOT_KNOWN.rows; ++i) {
@@ -488,12 +620,15 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
         }
 
         cv::Mat zoom3;
-        cv::resize(NOT_KNOWN, zoom3, cv::Size(), 4*f, 4*f, cv::INTER_NEAREST);
-
+        cv::resize(NOT_KNOWN, zoom3, cv::Size(), mul*f, mul*f, cv::INTER_NEAREST);
+	cv::medianBlur(zoom3, zoom3, 9);
+	cv::medianBlur(zoom3, zoom3, 5);
+	cv::medianBlur(zoom3, zoom3, 3);
         //cv::Mat BLAN_known_area3 = cv::Mat::zeros(zoom3.size(), CV_8UC1);
         //cv::medianBlur(zoom3, BLAN_known_area3, 3);
         //cv::blur(zoom3, BLAN_known_area3, cv::Size(5,5));
         //cv::GaussianBlur(zoom3, BLAN_known_area3, cv::Size(3,3), 0, 0);
+	//imshow("zoom3", zoom3);
 
         cv::Mat color_map3 = cv::Mat::zeros(zoom3.size(), CV_8UC3);
 
@@ -518,7 +653,72 @@ cv::Mat Geo_area(int* data, int width, int height, int* path_x, int* path_y, int
             }
         }
 
-	return color_map3;
+	Mat srcI;
+	Mat dstI = color_map3.clone();
+	threshold(zoom3, srcI, 100, 255, CV_THRESH_BINARY); //二值化
+	srcI = ~srcI;
+	//imshow("srcI", srcI);
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarcy;
+	findContours(srcI, contours, hierarcy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	vector<Rect> boundRect(contours.size());  //定义外接矩形集合
+	vector<RotatedRect> box(contours.size()); //定义最小外接矩形集合
+	Point2f rect[4];
+	double alpha;
+	int boxcenter_x, boxcenter_y;
+	for(int i = 0; i < contours.size(); i++)
+	{
+	    box[i] = minAreaRect(Mat(contours[i]));  //计算每个轮廓最小外接矩形
+	    alpha = fabs(box[i].angle) > 45 ? 90 - fabs(box[i].angle) : fabs(box[i].angle);
+    	    cout << alpha << endl;
+	    boxcenter_x = box[i].center.x;
+    	    boxcenter_y = box[i].center.y;
+	    boundRect[i] = boundingRect(Mat(contours[i]));
+	    circle(dstI, Point(box[i].center.x, box[i].center.y), 5, Scalar(0, 255, 0), -1, 8);  //绘制最小外接矩形的中心点
+	    box[i].points(rect);  //把最小外接矩形四个端点复制给rect数组
+	    //rectangle(dstI, Point(boundRect[i].x, boundRect[i].y), Point(boundRect[i].x + boundRect[i].width, boundRect[i].y + boundRect[i].height), Scalar(0, 255, 0), 2, 8);
+	    for(int j = 0; j < 4; j++)
+	    {
+		line(dstI, rect[j], rect[(j+1)%4], Scalar(0, 0, 255), 2, 8);  //绘制最小外接矩形每条边
+	    }
+	}
+	imshow("dstI", dstI);
+    	//imwrite("to_argb3.bmp", dstI);
+
+	vector<cv::Point2i> Points1;
+	vector<cv::Point2i> Points2;
+	//输出图像的尺寸与原图一样    
+	cv::Size dst_sz(dstI.cols, dstI.rows);
+	//指定旋转中心      
+	cv::Point2f center(boxcenter_x, boxcenter_y);
+	
+        for(int i = 0; i < color_map3.rows; ++i) {
+            for(int j = 0; j < color_map3.cols; ++j) {
+
+		if(color_map3.at<cv::Vec3b>(i, j)[0] == 247 && color_map3.at<cv::Vec3b>(i, j)[1] == 167 && color_map3.at<cv::Vec3b>(i, j)[2] == 205) {
+			Points1.push_back(Point(j, i));
+		}
+
+		if(color_map3.at<cv::Vec3b>(i, j)[0] == 245 && color_map3.at<cv::Vec3b>(i, j)[1] == 81 && color_map3.at<cv::Vec3b>(i, j)[2] == 116) {
+			Points2.push_back(Point(j, i));
+		}
+            }
+        }
+	Mat dstColor(dstI.size(), CV_8UC3, Scalar(241, 119, 160));
+	vector<cv::Point2i> dstPoints1 = getRotatePoint(dstColor, Points1, center, alpha);
+	for (size_t i = 0; i < dstPoints1.size(); i++)
+	{
+		cv::circle(dstColor, dstPoints1.at(i), 5, cv::Scalar(247, 167, 205), 2);
+	}
+	vector<cv::Point2i> dstPoints2 = getRotatePoint(dstColor, Points2, center, alpha);
+	for (size_t i = 0; i < dstPoints2.size(); i++)
+	{
+		cv::circle(dstColor, dstPoints2.at(i), 5, cv::Scalar(245, 81, 116), 2);
+	}
+	//cv::imshow("Rotation Image", dstColor);
+
+        //cv::blur(dstColor, dstColor, cv::Size(3,3));
+	return dstColor;
     }
 }
 
@@ -527,30 +727,37 @@ int main()
 {
     //例子：5（开区域） 和 map-1（闭区域）
     int data[MAXN], datalen = 0;
-    int width = 154, height = 212; // 5
-    //int width = 210, height = 172; // map-1
+    int width = 123, height = 80; // 5
 
     //获取地图数据
-    ifstream file("5.txt"); // 5 or map-1
+    ifstream file("map_s.txt"); // 5
     while( ! file.eof() )
     file >> data[datalen ++];
     file.close();
 
-    float x0 = -9.651299476623535; // 5
-    float y0 = -5.301003932952881; // 5 
-    //float x0 = -3.600197076797485; // map-1
-    //float y0 = -9.177140235900879; // map-1
+    //float x0 = -9.651299476623535; // 5---w = 154, h = 212
+    //float y0 = -5.301003932952881; // 5 
+    float x0 = -2.328948736190796; // map_s---w = 123, h = 80
+    float y0 = -5.480353355407715;
+    //float x0 = -1.621787786483765; // map_tw---w = 58, h = 81
+    //float y0 = -0.6075371503829956;
+    //float x0 = -4.292689800262451; // map_66---w = 183, h = 182
+    //float y0 = -7.424565315246582;
+    //float x0 = -4.471129894256592; // map_11---w = 159, h = 176
+    //float y0 = -5.659699440002441;
+    //float x0 = -3.600197076797485; // map_o---w = 210, h = 172
+    //float y0 = -9.177140235900879;
 
     //获得路线数据
     int path_x[MAXN/2], path_y[MAXN/2], len; 
     len = 0;
-    ifstream file_x("5x.txt"); // 5x or pathx_o
+    ifstream file_x("pathx_s.txt"); // 5x or pathx_o
     while( ! file_x.eof() )
     file_x >> path_x[len ++];
     file_x.close();
 
     len = 0;
-    ifstream file_y("5y.txt"); // 5y or pathy_o
+    ifstream file_y("pathy_s.txt"); // 5y or pathy_o
     while( ! file_y.eof() )
     file_y >> path_y[len ++];
     file_y.close();
@@ -558,11 +765,9 @@ int main()
     cv::Mat asrc = Geo_area(data, width, height, path_x, path_y, len, x0, y0, 1);
 
     imshow("asrc", asrc);
-    imwrite("to_argb2.bmp", asrc);
+    //imwrite("to_argb3-.bmp", asrc);
 
     cvWaitKey(0);
 
     return 0;
 }
-
-
